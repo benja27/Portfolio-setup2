0c964b9664fe75e93dd0a940b3ef449bfc1cf6b0
benja <rksnek@gmail.com>
2023-03-06 21:03:29 -0600

    add Linter files conf

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 
 // 
 let generator = document.querySelector('.header-menu-hamburguer')
-
 // 
+
 let target1 = document.querySelector('.header-menu-hamburguer img')
 let target2 = document.querySelector('.header-menu-hamburguer i')
 
 let modal = document.querySelector('.menu-mobil')
 
 let header = document.querySelector('.hero')
+
 let logo = document.querySelector('.header > p')
 
-console.log(target2)
+
+document.documentElement.addEventListener('click', e=>{
+  console.log(e.target)
+
+  if(e.target.matches('#uno')){
+    modal.classList.toggle('show')
+    header.classList.toggle('blur')
+   logo.classList.toggle('blur')
+  }
+  if(e.target.matches('#dos')){
+    modal.classList.toggle('show')
+    header.classList.toggle('blur')
+   logo.classList.toggle('blur')
+  }
+  if(e.target.matches('#tres')){
+    modal.classList.toggle('show')
+    header.classList.toggle('blur')
+   logo.classList.toggle('blur')
+  }
+})
 
 
 generator.addEventListener('click', ()=>{ 

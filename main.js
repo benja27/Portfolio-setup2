3ac2d35530963b0272e4e379cbd5af5dd9b3af33
benja <rksnek@gmail.com>
2023-03-07 11:04:52 -0600

    Add linter fixes

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
+//
+const generator = document.querySelector('.header-menu-hamburguer');
+//
 
-// 
-let generator = document.querySelector('.header-menu-hamburguer')
-// 
+const target1 = document.querySelector('.header-menu-hamburguer img');
+const target2 = document.querySelector('.header-menu-hamburguer i');
 
-let target1 = document.querySelector('.header-menu-hamburguer img')
-let target2 = document.querySelector('.header-menu-hamburguer i')
+const modal = document.querySelector('.menu-mobil');
 
-let modal = document.querySelector('.menu-mobil')
+const header = document.querySelector('.hero');
 
-let header = document.querySelector('.hero')
+const logo = document.querySelector('.header > p');
 
-let logo = document.querySelector('.header > p')
-
-
-document.documentElement.addEventListener('click', e=>{
-  console.log(e.target)
-
-  if(e.target.matches('#uno')){
-    modal.classList.toggle('show')
-    header.classList.toggle('blur')
-   logo.classList.toggle('blur')
+document.documentElement.addEventListener('click', (e) => {
+  if (e.target.matches('#uno')) {
+    modal.classList.toggle('show');
+    header.classList.toggle('blur');
+    logo.classList.toggle('blur');
+    target1.classList.toggle('hide');
+    target2.classList.toggle('show');
   }
-  if(e.target.matches('#dos')){
-    modal.classList.toggle('show')
-    header.classList.toggle('blur')
-   logo.classList.toggle('blur')
+  if (e.target.matches('#dos')) {
+    modal.classList.toggle('show');
+    header.classList.toggle('blur');
+    logo.classList.toggle('blur');
+    target1.classList.toggle('hide');
+    target2.classList.toggle('show');
   }
-  if(e.target.matches('#tres')){
-    modal.classList.toggle('show')
-    header.classList.toggle('blur')
-   logo.classList.toggle('blur')
+  if (e.target.matches('#tres')) {
+    modal.classList.toggle('show');
+    header.classList.toggle('blur');
+    logo.classList.toggle('blur');
+    target1.classList.toggle('hide');
+    target2.classList.toggle('show');
   }
-})
+});
 
-
-generator.addEventListener('click', ()=>{ 
-   target1.classList.toggle('hide') 
-   target2.classList.toggle('show') 
-   modal.classList.toggle('show')
-   header.classList.toggle('blur')
-   logo.classList.toggle('blur')
-})
+generator.addEventListener('click', () => {
+  target1.classList.toggle('hide');
+  target2.classList.toggle('show');
+  modal.classList.toggle('show');
+  header.classList.toggle('blur');
+  logo.classList.toggle('blur');
+});

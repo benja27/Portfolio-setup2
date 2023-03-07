f8f99e920bff69c6b473949a70783c80fb1b05ac
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-07 16:42:12 -0600

    Add linter fixes

--- main.js
+++ main.js
@@ -1,42 +1,15 @@
-const hamburguerContainer = document.querySelector('.header-menu-hamburguer');
-
 const hamburguerIcon = document.querySelector('.header-menu-hamburguer img');
 const closeIcon = document.querySelector('.header-menu-hamburguer i');
-
-const menuIcon = document.querySelector('.menu-mobil');
-
+const menuMobil = document.querySelector('.menu-mobil');
 const headerBlur = document.querySelector('.hero');
-
-const logoBlur = document.querySelector('.header > p');
+const logoBlur = document.querySelector('.header > a');
 
 document.documentElement.addEventListener('click', (e) => {
-  if (e.target.matches('#uno')) {
-    menuIcon.classList.toggle('show');
+  if ((e.target.matches('.menu-mobil a')) || (e.target.matches('.header-menu-hamburguer *'))) {
+    menuMobil.classList.toggle('show');
     headerBlur.classList.toggle('blur');
     logoBlur.classList.toggle('blur');
     hamburguerIcon.classList.toggle('hide');
     closeIcon.classList.toggle('show');
   }
-  if (e.target.matches('#dos')) {
-    menuIcon.classList.toggle('show');
-    headerBlur.classList.toggle('blur');
-    logoBlur.classList.toggle('blur');
-    hamburguerIcon.classList.toggle('hide');
-    closeIcon.classList.toggle('show');
-  }
-  if (e.target.matches('#tres')) {
-    menuIcon.classList.toggle('show');
-    headerBlur.classList.toggle('blur');
-    logoBlur.classList.toggle('blur');
-    hamburguerIcon.classList.toggle('hide');
-    closeIcon.classList.toggle('show');
-  }
-});
-
-hamburguerContainer.addEventListener('click', () => {
-  hamburguerIcon.classList.toggle('hide');
-  closeIcon.classList.toggle('show');
-  menuIcon.classList.toggle('show');
-  headerBlur.classList.toggle('blur');
-  logoBlur.classList.toggle('blur');
 });

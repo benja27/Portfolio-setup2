9c054143a882facc5c73013a5ca00f4f7a07614a
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-09 15:10:50 -0600

    Add linter fixes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import openMenu from './js/hamburguer.js';
 import createCardProject from './js/works.js';
-import { createModal } from './js/modalWindow.js';
+import createModal from './js/modalWindow.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   createCardProject();

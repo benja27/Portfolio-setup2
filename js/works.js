5c8d9fa70ef1457b797e401dd39d2aa5cf8e9227
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-09 12:36:12 -0600

    Add modal window

---
+++ js/works.js
@@ -0,0 +1,107 @@
+// let reverse;
+
+// const cardsinfo = [
+//   {
+//     titulo: 'tonic',
+//     subtitulo: 'CANOPY',
+//     description: 'asdfasdfasd',
+//   },
+// ];
+
+// const card = `<article class="work ">
+// <img
+//   class="work-main-img"
+//   alt="image of the first proyect"
+//   src="./assets/Snapshoot Portfolio.svg"
+//   alt=""
+// />
+// <aside class="work-info">
+//   <h2 class="work-title">Tonic</h2>
+//   <ul class="work-list">
+//     <h4> ${cardsinfo[0].subtitulo} </h4>
+//     <li class="work-list-item">
+//       <img src="./assets/Counter.svg" alt="" />
+//       Back End Dev
+//     </li>
+//     <li class="work-list-item">
+//       <img src="./assets/Counter.svg" alt="" />
+//       2015
+//     </li>
+//   </ul>
+//   <article class="work-description">
+//     <p>
+//       ${cardinfo.description}
+//     </p>
+//   </article>
+//   <ul class="work-tech">
+//     <li>html</li>
+//     <li>css</li>
+//     <li>javaScript</li>
+//   </ul>
+//   <article class="work-button">
+//     <button type="button" class="btn">See project</button>
+//   </article>
+// </aside>
+// </article>`;
+
+// const container = document.querySelector('.works-container');
+
+// const div = document.createElement('div');
+
+// div.innerHTML = card;
+
+// container.appendChild();
+
+// console.log(card);
+
+// const fragment = document.createDocumentFragment();
+
+// const worksContainer = document.querySelector('.works-container');
+
+// export function createCard(reverse) {
+//   const card = `<article class="work work-${reverse}">
+// <img
+//   class="work-main-img"
+//   alt="image of the first proyect"
+//   src="./assets/Snapshoot Portfolio.svg"
+//   alt=""
+// />
+// <aside class="work-info">
+//   <h2 class="work-title">Tonic</h2>
+//   <ul class="work-list">
+//     <h4>CANOPY</h4>
+//     <li class="work-list-item">
+//       <img src="./assets/Counter.svg" alt="" />
+//       Back End Dev
+//     </li>
+//     <li class="work-list-item">
+//       <img src="./assets/Counter.svg" alt="" />
+//       2015
+//     </li>
+//   </ul>
+//   <article class="work-description">
+//     <p>
+//       A daily selection of privately personalized reads; no accounts
+//       or sign-ups required.
+//     </p>
+//   </article>
+//   <ul class="work-tech">
+//     <li>html</li>
+//     <li>css</li>
+//     <li>javaScript</li>
+//   </ul>
+//   <article class="work-button">
+//     <button type="button" class="btn">See project</button>
+//   </article>
+// </aside>
+// </article>`;
+//   const fragment = document.createDocumentFragment();
+//   const worksContainer = document.querySelector('.works-container');
+
+//   fragment.append(card);
+// }
+
+// const card1 = {
+//   src: './assets/Snapshoot Portfolio.svg',
+//   titulo: 'topic',
+// };
--- main.js
+++ main.js
@@ -1,15 +1,91 @@
-const hamburguerIcon = document.querySelector('.header-menu-hamburguer img');
-const closeIcon = document.querySelector('.header-menu-hamburguer i');
-const menuMobil = document.querySelector('.menu-mobil');
-const headerBlur = document.querySelector('.hero');
-const logoBlur = document.querySelector('.header > a');
+import openMenu from './js/hamburguer.js';
 
 document.documentElement.addEventListener('click', (e) => {
   if ((e.target.matches('.menu-mobil a')) || (e.target.matches('.header-menu-hamburguer *'))) {
-    menuMobil.classList.toggle('show');
-    headerBlur.classList.toggle('blur');
-    logoBlur.classList.toggle('blur');
-    hamburguerIcon.classList.toggle('hide');
-    closeIcon.classList.toggle('show');
+    openMenu();
   }
 });
+
+// prueba -----------------------------------------
+
+const Arraycards = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: './assets/Snapshoot Portfolio.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
+    link_live_version: 'https://www.google.com',
+    link_to_source: 'https://www.google.com',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: './assets/Snapshoot Portfolio2.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
+    link_live_version: 'https://www.google.com',
+    link_to_source: 'https://www.google.com',
+  },
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: './assets/Snapshoot Portfolio3.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
+    link_live_version: 'https://www.google.com',
+    link_to_source: 'https://www.google.com',
+  },
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: './assets/Snapshoot Portfolio4.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
+    link_live_version: 'https://www.google.com',
+    link_to_source: 'https://www.google.com',
+  },
+
+];
+
+function cbody(c) {
+  const cardbody = `<img
+  class="work-main-img"
+  alt="image of the first proyect"
+  src=" ${c.featuredImage} "
+  alt=""
+/>
+<aside class="work-info">
+  <h2 class="work-title"> ${c.name}    </h2>
+  <ul class="work-list">
+    <h4>CANOPY</h4>
+    <li class="work-list-item">
+      <img src="./assets/Counter.svg" alt="" />
+      Back End Dev
+    </li>
+    <li class="work-list-item">
+      <img src="./assets/Counter.svg" alt="" />
+      2015
+    </li>
+  </ul>
+  <article class="work-description">
+    <p>
+       ${c.description}
+    </p>
+  </article>
+  <ul class="work-tech">
+    <li> ${c.technologies[0]} </li>
+    <li> ${c.technologies[1]} </li>
+    <li> ${c.technologies[2]} </li>
+  </ul>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+See project
+</button>
+</aside>`;
+
+  return cardbody;
+}
+
+for (let i = 0; i < Arraycards.length; i += 1) {
+  const div = document.createElement('article');
+  div.className = 'work';
+  div.innerHTML = cbody(Arraycards[i]);
+  const contenedor = document.querySelector('.works-container');
+  contenedor.appendChild(div);
+}

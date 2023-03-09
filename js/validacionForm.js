84c92a0c07e9a1262401489098c60d6deaccb3d6
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-09 17:17:50 -0600

    Add validation function

--- js/validacionForm.js
+++ js/validacionForm.js
@@ -1,3 +1,24 @@
-let contact = document.querySelector('.contact-form')
+export default function validation(){
+  let form = document.querySelector('.contact-form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let email = document.getElementById('email').value
+  
+    console.log(email)
+    let regex = /^[a-z@.]+$/
+  
+    if (regex.test(email)){
+      form.submit()
+    }else{
+      let error = document.querySelector('.email-error')
+      error.classList.toggle('d-none')
+    }
+  })
+}
 
-console.log(contact)
+
+
+
+
+
+

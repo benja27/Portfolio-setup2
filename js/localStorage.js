912ade841df68ab9b35d4fedc7498ae6c0ecfedb
benja27 <50673336+benja27@users.noreply.github.com>
2023-03-11 15:33:40 -0600

    Add linter fixes

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,4 +1,4 @@
-export default function setInfo() {
+export const setInfo = () => {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const coments = document.getElementById('comment').value;
@@ -8,15 +8,13 @@ export default function setInfo() {
     coments,
   };
   localStorage.setItem('formInfo', JSON.stringify(info));
-}
+};
 
-export function getInfo() {
+export const getInfo = () => {
   const data = JSON.parse(localStorage.getItem('formInfo'));
   if (data.name !== '') {
     document.getElementById('name').value = data.name;
     document.getElementById('email').value = data.email;
     document.getElementById('comment').value = data.coments;
   }
-}
-
-export { setInfo };
+};

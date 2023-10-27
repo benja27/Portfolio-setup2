feb394388afb36679573d21d5680473c536c2a3d
benja27 <50673336+benja27@users.noreply.github.com>
2023-10-26 22:41:35 -0600

    update portfolio

--- js/hamburguer.js
+++ js/hamburguer.js
@@ -3,10 +3,12 @@ const closeIcon = document.querySelector('.header-menu-hamburguer i');
 const menuMobil = document.querySelector('.menu-mobil');
 const headerBlur = document.querySelector('.hero');
 const logoBlur = document.querySelector('.header > a');
+const header = document.querySelector('.header');
 
 export default function openMenu() {
   menuMobil.classList.toggle('show');
   headerBlur.classList.toggle('blur');
+  header.classList.toggle('sticky');
   logoBlur.classList.toggle('blur');
   hamburguerIcon.classList.toggle('hide');
   closeIcon.classList.toggle('show');
